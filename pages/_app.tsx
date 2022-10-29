0b33f93c804d504c2f0460577bac9ca9335dda99
Chang Shen Ho <vulgal@gmail.com>
2022-10-29 21:57:15 +0100

    Get rid of CSS cuz it sucked + add 25:00 mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,47 @@
-import '../styles/globals.css'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+import React from 'react'
+
+import '@fontsource/fira-mono'
+import '@fontsource/fira-sans'
+
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export const ColorModeContext = React.createContext({ setIsDark: (dark: boolean) => {}, toggleColorMode: () => {} })
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const [mode, setMode] = React.useState<'light' | 'dark'>('light')
+  const colorMode = React.useMemo(
+    () => ({
+      setIsDark: (dark: boolean): void => setMode(dark ? 'dark' : 'light'),
+      toggleColorMode: (): void => {
+        setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'))
+      },
+    }),
+    [],
+  )
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+          primary: {
+            main: mode === 'dark' ? '#953752' : '#33A4EB',
+          },
+        },
+        typography: {
+          fontFamily: 'Fira Sans, sans-serif',
+        },
+      }),
+    [mode],
+  )
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
 }
